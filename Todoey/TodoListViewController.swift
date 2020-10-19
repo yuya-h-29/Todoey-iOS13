caf2ba361211a053812dffba7df896e7f77adf7a
yuya-h-29 <tzou11291996@gmail.com>
2020-10-19 23:56:15 +0900

    add cell to table view

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Philipp Muellauer on 02/12/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["aaa", "bbbb", "cccc"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+
+    //MARK: - tableview datasource
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+
+    
+
+}
+
